4ac2fcd0377251115e4cfd7c9c172100f18b6dc8
Ryan Duryea <aguynamedryan@gmail.com>
2014-01-02 17:57:17 -0800

    Add bulk of changes to make app work

--- app.js
+++ app.js
@@ -4,10 +4,9 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var GratitudeProvider = require('./gratitudeprovider.js').GratitudeProvider;
 
 var app = express();
 
@@ -15,9 +14,11 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view options', {layout: false});
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
+app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
@@ -29,8 +30,34 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var gratitudeProvider = new GratitudeProvider('localhost', 27017)
+
+app.locals.moment = require('moment');
+
+// Routes
+
+app.get('/', function(req, res) {
+  gratitudeProvider.findAll(function(error, grats) {
+    res.render('index', {
+      title: "What You're Grateful For",
+      gratitudes: grats
+    });
+  });
+});
+
+app.get('/gratitude/new', function(req, res) {
+  res.render('gratitude_new', {
+    title: 'New Thanks'
+  });
+});
+
+app.post('/gratitude/new', function(req, res) {
+  gratitudeProvider.save({
+    what: req.param('what')
+  }, function(error, docs) {
+    res.redirect('/')
+  });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
